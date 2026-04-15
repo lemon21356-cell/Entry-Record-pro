516405c1fe2bd40cc5711bbaa719c9c3ed2e49c7
lemon21356-cell <lemon21356@gmail.com>
2026-04-15 19:08:46 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "download_video") {
+    if (request.action === "download_request") {
         chrome.downloads.download({
             url: request.url,
-            // 크롬 기본 다운로드 폴더 내의 'Entryrecord' 폴더로 경로 강제
-            filename: "Entryrecord/" + request.filename,
+            filename: request.filename, // 크롬 설정에서 지정한 폴더로 바로 저장됨
             saveAs: false
+        }, (downloadId) => {
+            if (chrome.runtime.lastError) {
+                console.error("다운로드 실패:", chrome.runtime.lastError.message);
+            }
         });
     }
 });
